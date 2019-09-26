60bd2b9a27e2b4c5f91c8e1750ce01af27f58576
Steffen Graber <s.graber@fz-juelich.de>
2019-09-26 09:40:21 +0200

    disable MPI installation

--- recipe/build.sh
+++ recipe/build.sh
@@ -44,8 +44,8 @@ fi
 if [[ $(uname) == Darwin ]]; then
 	cmake -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
 		  -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
-		  -Dwith-mpi=${mpi_arg} \
-		  -Dwith-openmp=${mpi_arg} \
+		  -Dwith-mpi=NO \
+		  -Dwith-openmp=NO \
 		  -Dwith-python=3 \
 		  -DPYTHON_EXECUTABLE=${PYTHON}\
 		  -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.dylib \
