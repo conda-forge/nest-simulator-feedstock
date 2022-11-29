89a9b2859922bea3133d94dfd284a542924ab077
Steffen Graber <s.graber@fz-juelich.de>
2022-11-29 08:41:44 +0100

    Add html doc

--- recipe/build.sh
+++ recipe/build.sh
@@ -19,6 +19,9 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
   popd
 fi
 
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade  Building NEST documentation PyYAML>=4.2b1 assimulo breathe csvkit docutils doxygen mock nbsphinx numpydoc path.py pydot recommonmark sphinx >= 4.0.2 sphinx_rtd_theme tqdm yamllint
+
 mkdir build
 cd build
 
