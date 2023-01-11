a001e541ceaa8e53f32177f27793e54c481254cb
Steffen Graber <s.graber@fz-juelich.de>
2023-01-11 14:57:37 +0100

    Fix prefix path for run env

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-source "${PREFIX}/bin/nest_vars.sh"
+source "${CONDA_PREFIX}/bin/nest_vars.sh"
 
 COMPILER_FULL=$(nest-config --compiler)
 COMPILER_NAME=$(basename "${COMPILER_FULL}")
+COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
 
-perl -i -pe's/${COMPILER_FULL}/${CONDA_PREFIX}\/bin\/${COMPILER_NAME}/' "${PREFIX}/bin/nest-config"
+perl -i -pe's/${COMPILER_FULL}/${COMPILER_RUN}/' "${CONDA_PREFIX}/bin/nest-config"
