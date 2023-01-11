c407b7acbf1060820fd17838754b080879daf068
Steffen Graber <s.graber@fz-juelich.de>
2023-01-11 13:47:17 +0100

    Fix problem with sed -i on OSX. Replace with perl.

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -4,4 +4,4 @@ source "${PREFIX}/bin/nest_vars.sh"
 COMPILER_FULL=$(nest-config --compiler)
 COMPILER_NAME=$(basename "${COMPILER_FULL}")
 
-sed -i "s!${COMPILER_FULL}!${CONDA_PREFIX}/bin/${COMPILER_NAME}!" "${PREFIX}/bin/nest-config"
+perl -i -pe's/${COMPILER_FULL}/${CONDA_PREFIX}\/bin\/${COMPILER_NAME}/' "${PREFIX}/bin/nest-config"
