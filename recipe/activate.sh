60078f66a9133f6b31a05160cde513080c21d354
Steffen Graber <s.graber@fz-juelich.de>
2023-01-11 10:43:37 +0100

    Fix compiler issues in run env

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+source "${PREFIX}/bin/nest_vars.sh"
 
-source ${CONDA_PREFIX}/bin/nest_vars.sh
+COMPILER_FULL=$(nest-config --compiler)
+COMPILER_NAME=$(basename "${COMPILER_FULL}")
+
+sed -i "s!${COMPILER_FULL}!${CONDA_PREFIX}/bin/${COMPILER_NAME}!" "${PREFIX}/bin/nest-config"
