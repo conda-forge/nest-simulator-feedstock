47e4b948807e14c4fa318f59d8b6b4c26d8bd52c
Steffen Graber <s.graber@fz-juelich.de>
2023-01-30 11:08:21 +0100

    Replace source because not every env have it in place

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-source "${CONDA_PREFIX}/bin/nest_vars.sh"
+. "${CONDA_PREFIX}/bin/nest_vars.sh"
 
 COMPILER_FULL=$(nest-config --compiler)
 COMPILER_NAME=$(basename "${COMPILER_FULL}")
 COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
 
-perl -i -pe's/${COMPILER_FULL}/${COMPILER_RUN}/' "${CONDA_PREFIX}/bin/nest-config"
+perl -i -pe 's/${COMPILER_FULL}/${COMPILER_RUN}/' "${CONDA_PREFIX}/bin/nest-config"
