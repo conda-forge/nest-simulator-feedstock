b66ccd65f39d53775514f275247afc9db8f33c44
Steffen Graber <s.graber@fz-juelich.de>
2023-02-13 09:18:35 +0100

    Fix error:  -I or -i may not be used with stdin

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -5,4 +5,4 @@ COMPILER_FULL=$(nest-config --compiler)
 COMPILER_NAME=$(basename "${COMPILER_FULL}")
 COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
 
-sed -i "s|$COMPILER_FULL|$COMPILER_RUN|g" $CONDA_PREFIX/bin/nest-config
+sed "s|$COMPILER_FULL|$COMPILER_RUN|g" $CONDA_PREFIX/bin/nest-config
