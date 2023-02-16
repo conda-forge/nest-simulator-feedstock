340a1a99c64a28a93b32d459daf9d6891f841a76
Steffen Graber <s.graber@fz-juelich.de>
2023-02-16 11:52:20 +0100

    Fix differences between BSD/macOS and GNU/Linux sed syntax

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -8,7 +8,13 @@ COMPILER_RUN=$(which c++)
 
 sed "s|$COMPILER_FULL|$COMPILER_RUN|g" "$CONDA_PREFIX"/bin/nest-config
 
+if [[ $OSTYPE == 'linux'* ]]; then
+  sed -i "s|$COMPILER_FULL|$COMPILER_RUN|g" "$CONDA_PREFIX"/bin/nest-config
+  cat "$CONDA_PREFIX"/bin/nest-config
+fi
 
 if [[ $OSTYPE == 'darwin'* ]]; then
+  sed "s|$COMPILER_FULL|$COMPILER_RUN|g" "$CONDA_PREFIX"/bin/nest-config > "$CONDA_PREFIX"/bin/nest-config
   sed "s|-fopenmp=libomp|-Xclang -fopenmp|g" "$CONDA_PREFIX"/bin/nest-config
+  cat "$CONDA_PREFIX"/bin/nest-config
 fi
