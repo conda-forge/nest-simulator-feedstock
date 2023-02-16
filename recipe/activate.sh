f42357bc4d5ef27e2d5dcb75cf01aa276afe172d
Steffen Graber <s.graber@fz-juelich.de>
2023-02-16 10:56:07 +0100

    Add sed replace in nest-config in osx

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -3,6 +3,12 @@
 
 COMPILER_FULL=$(nest-config --compiler)
 COMPILER_NAME=$(basename "${COMPILER_FULL}")
-COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
+# COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
+COMPILER_RUN=$(which c++)
 
 sed "s|$COMPILER_FULL|$COMPILER_RUN|g" "$CONDA_PREFIX"/bin/nest-config
+
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  sed "s|-fopenmp=libomp|-Xclang -fopenmp|g" "$CONDA_PREFIX"/bin/nest-config
+fi
