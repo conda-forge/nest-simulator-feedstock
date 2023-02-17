af676ff0a59ec7f227db146dbb316e37319f3877
Steffen Graber <s.graber@fz-juelich.de>
2023-02-17 08:26:46 +0100

    Work on mac error: /bin/nest-config: Permission denied

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -12,10 +12,10 @@ if [[ ${OSTYPE} == 'linux'* ]]; then
   cat "${CONDA_PREFIX}"/bin/nest-config
 fi
 
-if [[ ${OSTYPE} == 'darwin'* ]]; then
-  sed "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config > "${CONDA_PREFIX}"/bin/nest-config-tmp
-  mv "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config-bac
-  mv "${CONDA_PREFIX}"/bin/nest-config-tmp "${CONDA_PREFIX}"/bin/nest-config
-  sed "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
-  cat "${CONDA_PREFIX}"/bin/nest-config
-fi
+#if [[ ${OSTYPE} == 'darwin'* ]]; then
+#  sed "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config > "${CONDA_PREFIX}"/bin/nest-config-tmp
+#  mv "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config-bac
+#  mv "${CONDA_PREFIX}"/bin/nest-config-tmp "${CONDA_PREFIX}"/bin/nest-config
+#  sed "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
+#  cat "${CONDA_PREFIX}"/bin/nest-config
+#fi
