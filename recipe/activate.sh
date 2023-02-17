584e101a578553e01c091d5126d39e6a4406dc0e
Steffen Graber <s.graber@fz-juelich.de>
2023-02-17 07:12:21 +0100

    Add repalcing

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -5,6 +5,17 @@ COMPILER_FULL=$(nest-config --compiler)
 COMPILER_NAME=$(basename "${COMPILER_FULL}")
 COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
 
-sed "s|$COMPILER_FULL|$COMPILER_RUN|g" "$CONDA_PREFIX"/bin/nest-config
+# sed "s|$COMPILER_FULL|$COMPILER_RUN|g" "$CONDA_PREFIX"/bin/nest-config
 
+if [[ ${OSTYPE} == 'linux'* ]]; then
+  sed -i "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
+  cat "${CONDA_PREFIX}"/bin/nest-config
+fi
 
+if [[ ${OSTYPE} == 'darwin'* ]]; then
+  sed "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config > "${CONDA_PREFIX}"/bin/nest-config-tmp
+  mv "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config-bac
+  mv "${CONDA_PREFIX}"/bin/nest-config-tmp "${CONDA_PREFIX}"/bin/nest-config
+  sed "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
+  cat "${CONDA_PREFIX}"/bin/nest-config
+fi
