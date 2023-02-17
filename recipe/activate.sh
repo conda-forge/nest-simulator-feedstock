60f4b75b93c1353a5e7f057bb4ad9daae9e8ce49
Steffen Graber <s.graber@fz-juelich.de>
2023-02-17 15:00:42 +0100

    Select OSTYPE

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -7,15 +7,15 @@ COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
 
 # sed "s|$COMPILER_FULL|$COMPILER_RUN|g" "$CONDA_PREFIX"/bin/nest-config
 
-if [[ ${OSTYPE} == 'linux'* ]]; then
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sed -i "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
   cat "${CONDA_PREFIX}"/bin/nest-config
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  sed "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config > "${CONDA_PREFIX}"/bin/nest-config-tmp
+  mv "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config-bac
+  mv "${CONDA_PREFIX}"/bin/nest-config-tmp "${CONDA_PREFIX}"/bin/nest-config
+  sed "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
+  cat "${CONDA_PREFIX}"/bin/nest-config
+else
+  echo "$OSTYPE"
 fi
-
-#if [[ ${OSTYPE} == 'darwin'* ]]; then
-#  sed "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config > "${CONDA_PREFIX}"/bin/nest-config-tmp
-#  mv "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config-bac
-#  mv "${CONDA_PREFIX}"/bin/nest-config-tmp "${CONDA_PREFIX}"/bin/nest-config
-#  sed "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
-#  cat "${CONDA_PREFIX}"/bin/nest-config
-#fi
