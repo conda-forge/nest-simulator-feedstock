07762724ba78f72ee8f3173d4f9fc6692f7ffa25
Steffen Graber <s.graber@fz-juelich.de>
2023-02-23 11:07:52 +0000

    Execute after installation of NEST

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,28 +1,3 @@
 #!/bin/bash
+
 . "${CONDA_PREFIX}/bin/nest_vars.sh"
-
-COMPILER_FULL=$(bash nest-config --compiler)
-COMPILER_NAME=$(basename "${COMPILER_FULL}")
-COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  echo "COMPILER LINUX BUILD"
-  bash nest-config --compiler
-  sed -i "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
-  echo "COMPILER LINUX RUN"
-  bash nest-config --compiler
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  echo "COMPILER DARWIN BUILD"
-  bash nest-config --compiler
-  sed "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config > "${CONDA_PREFIX}"/bin/nest-config-tmp
-  sed "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config-tmp > "${CONDA_PREFIX}"/bin/nest-config-tmp-2  
-  mv "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config-bac
-  mv "${CONDA_PREFIX}"/bin/nest-config-tmp-2 "${CONDA_PREFIX}"/bin/nest-config
-  echo "COMPILER DARWIN RUN"
-  bash nest-config --compiler
-  cat "${CONDA_PREFIX}"/bin/nest-config
-else
-  echo "$OSTYPE"
-fi
- 
-
