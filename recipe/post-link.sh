bb1c6c991f90aeb01cbad3919d7ecf4ac4c217b7
Steffen Graber <s.graber@fz-juelich.de>
2023-02-23 11:45:06 +0000

    Should not write to stdout or stderr but to $PREFIX/.messages.txt

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -7,23 +7,23 @@ COMPILER_NAME=$(basename "${COMPILER_FULL}")
 COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  echo "COMPILER LINUX BUILD"
-  bash nest-config --compiler
+  echo "COMPILER LINUX BUILD" >> $PREFIX/.messages.txt
+  bash nest-config --compiler >> $PREFIX/.messages.txt
   sed -i "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
-  echo "COMPILER LINUX RUN"
-  bash nest-config --compiler
+  echo "COMPILER LINUX RUN" >> $PREFIX/.messages.txt
+  bash nest-config --compiler >> $PREFIX/.messages.txt
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  echo "COMPILER DARWIN BUILD"
-  bash nest-config --compiler
+  echo "COMPILER DARWIN BUILD" >> $PREFIX/.messages.txt
+  bash nest-config --compiler >> $PREFIX/.messages.txt
   sed "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config > "${CONDA_PREFIX}"/bin/nest-config-tmp
   sed "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config-tmp > "${CONDA_PREFIX}"/bin/nest-config-tmp-2  
   mv "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config-bac
   mv "${CONDA_PREFIX}"/bin/nest-config-tmp-2 "${CONDA_PREFIX}"/bin/nest-config
-  echo "COMPILER DARWIN RUN"
-  bash nest-config --compiler
-  cat "${CONDA_PREFIX}"/bin/nest-config
+  echo "COMPILER DARWIN RUN" >> $PREFIX/.messages.txt
+  bash nest-config --compiler >> $PREFIX/.messages.txt
+  cat "${CONDA_PREFIX}"/bin/nest-config >> $PREFIX/.messages.txt
 else
-  echo "$OSTYPE"
+  echo "$OSTYPE" >> $PREFIX/.messages.txt
 fi
  
 
