d50d0909a1e0779fa9659fe4707c084837f67e24
Steffen Graber <s.graber@fz-juelich.de>
2023-02-24 08:37:12 +0000

    Fix variable

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -4,8 +4,8 @@
 
 COMPILER_FULL=$(bash nest-config --compiler)
 COMPILER_NAME=$(basename "${CXX")
-# COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
-COMPILER_RUN=$(which ${COMPILER_NAME})
+COMPILER_RUN="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
+# COMPILER_RUN=$(which ${COMPILER_NAME})
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "COMPILER LINUX BUILD" >> $PREFIX/.messages.txt
