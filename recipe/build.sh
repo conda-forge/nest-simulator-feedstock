55431a8596c4b1fe023e802eceb69a7f8bf4aebe
Steffen Graber <s.graber@fz-juelich.de>
2023-06-28 08:18:57 +0000

    Disable cythinize when cross compiles

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,27 +12,26 @@ else
   CC=$(basename "${GCC}")
 fi
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-  export CMAKE_ARGS="${CMAKE_ARGS} -Dcythonize-pynest=OFF -DRUN_RESULT=0 -DRUN_RESULT__TRYRUN_OUTPUT:STRING="""
-  pushd pynest || return
-  ${PYTHON} -m cython pynestkernel.pyx --cplus
-  popd || return
-fi
+# if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+#   export CMAKE_ARGS="${CMAKE_ARGS} -Dcythonize-pynest=OFF -DRUN_RESULT=0 -DRUN_RESULT__TRYRUN_OUTPUT:STRING="""
+#   pushd pynest || return
+#   ${PYTHON} -m cython pynestkernel.pyx --cplus
+#   popd || return
+# fi
 
-mkdir build
-cd build || exit
-
-cmake "${CMAKE_ARGS}" -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
-        -Dwith-boost=ON \
-        -DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}" \
-        -Dwith-openmp=ON \
-        -Dwith-python=ON \
-        -DPYTHON_EXECUTABLE="${PYTHON}" \
-        -DPython_EXECUTABLE="${PYTHON}" \
-        -Dwith-gsl="${PREFIX}" \
-        -DREADLINE_ROOT_DIR="${PREFIX}" \
-        -DLTDL_ROOT_DIR="${PREFIX}" \
-..
+mkdir ../build
+pushd ../build || exit
+cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+      -Dwith-boost=ON \
+      -DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}" \
+      -Dwith-openmp=ON \
+      -Dwith-python=ON \
+      -Dwith-hdf5=ON \
+      -DPYTHON_EXECUTABLE="${PYTHON}" \
+      -Dwith-gsl="${PREFIX}" \
+      -DREADLINE_ROOT_DIR="${PREFIX}" \
+      -DLTDL_ROOT_DIR="${PREFIX}" \
+${SRC_DIR}
 
 make -j"${CPU_COUNT}"
 make install
