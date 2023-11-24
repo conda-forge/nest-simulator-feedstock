384cf960b41bd14b2d4582b412786a06891e2cb4
Steffen Graber <s.graber@fz-juelich.de>
2023-11-24 10:28:16 +0000

    Deactivate cythonize

--- recipe/build.sh
+++ recipe/build.sh
@@ -29,6 +29,7 @@ CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} -Dwith-boost=ON \
     -DCMAKE_FIND_APPBUNDLE=NEVER"
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+   echo "CONDA_BUILD_CROSS_COMPILATION changes"
    export CMAKE_ARGS="${CMAKE_ARGS} -Dcythonize-pynest=OFF -DRUN_RESULT=0 -DRUN_RESULT__TRYRUN_OUTPUT:STRING="""
    pushd pynest || return
    ${PYTHON} -m cython pynestkernel.pyx --cplus
