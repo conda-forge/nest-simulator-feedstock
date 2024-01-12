437ce098a08e4ef4d4f4e057596b990814ff5b4b
Steffen Graber <s.graber@fz-juelich.de>
2024-01-12 10:34:26 +0000

    Direct path replacement

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -4,12 +4,13 @@ COMPILER_FULL=$(bash nest-config --compiler)
 COMPILER_NAME=$(basename "${CXX}")
 COMPILER_RUN=$(which "${COMPILER_NAME}")
 
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sed -i "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   cat "${CONDA_PREFIX}"/bin/nest-config | sed "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
   cat "${CONDA_PREFIX}"/bin/nest-config | sed "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
-  cat "${CONDA_PREFIX}"/bin/nest-config | sed "s|${COMPILER_FULL}|${CONDA_PREFIX}/bin/clang++|g" "${CONDA_PREFIX}"/bin/nest-config
+  cat "${CONDA_PREFIX}"/bin/nest-config | sed "s|/Users/runner/miniforge3/conda-bld/nest-simulator.*clang++|${CONDA_PREFIX}/bin/clang++|g" "${CONDA_PREFIX}"/bin/nest-config
 else
   echo $OSTYPE
 fi
