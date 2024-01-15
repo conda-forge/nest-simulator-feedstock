0d7d0962052612bc1d940dbc0c90f90cb917f458
Steffen Graber <s.graber@fz-juelich.de>
2024-01-15 13:24:57 +0000

    Fix macos sed

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -9,9 +9,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   cp "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config.orig
   sed -i "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  cp "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config.orig
-  sed -i'sy' "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
-  sed -i'sy' "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
+  sed -i'.orig' "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
+  sed -i'.orig' "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
 else
   echo $OSTYPE
 fi
