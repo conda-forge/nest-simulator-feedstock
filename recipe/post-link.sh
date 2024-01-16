9e3dfc19760230a2dee4ce035992c0afa5eaf529
Steffen Graber <s.graber@fz-juelich.de>
2024-01-16 13:06:20 +0000

    Documentation

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -7,16 +7,15 @@ COMPILER_NAME=$(basename "${CXX}")
 COMPILER="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
 
 # To use nest with nestml in linux install
-#     mamba install -c conda-forge gcc_impl_linux-64 gcc_linux-64 \
-#           gxx_impl_linux-64 gxx_linux-64
+#     mamba install -c conda-forge gcc_impl_linux-64 gcc_linux-64 gxx_impl_linux-64 gxx_linux-64
 #
 # To use nest with nestml in osx-64 install
-#     mamba install -c conda-forge clang clang-16 clang_impl_osx-64 \
-#           clang_osx-64 clangxx clangxx_impl_osx-64 clangxx_osx-64
+#     mamba install -c conda-forge clangxx clangxx_impl_osx-64 clangxx_osx-64
 #
 # To use nest with nestml in osx-arm64 install
-#     mamba install -c conda-forge clang clang-16 clang_impl_osx-arm64 \
-#           clang_osx-arm64 clangxx clangxx_impl_osx-arm64 clangxx_osx-arm64
+#     mamba install -c conda-forge clangxx clangxx_impl_osx-arm64 clangxx_osx-arm64
+#
+# Update `nest-config.sh` with `.nest-simulator-post-link.sh`
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   cp "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config-"${now}".orig
