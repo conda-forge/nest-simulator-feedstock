cbc31cb94843104f7c4080229e00a6cb7ef00153
Steffen Graber <s.graber@fz-juelich.de>
2024-01-16 09:28:26 +0000

    New sed and doc

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -2,18 +2,31 @@
 
 COMPILER_FULL=$(bash nest-config --compiler)
 COMPILER_NAME=$(basename "${CXX}")
-COMPILER_RUN=$(which "${COMPILER_NAME}")
+#COMPILER_RUN=$(which "${COMPILER_NAME}")
+COMPILER="${CONDA_PREFIX}/bin/${COMPILER_NAME}"
 
-MAC_BLD_ENV="/Users/runner/miniforge3/conda-bld/nest-simulator_"
-MAC_BLD_COMPILER="/_build_env/bin/x86_64-apple-darwin*-clang++"
-MAC_COMPILER="${CONDA_PREFIX}/bin/clang++"
+# To use nest with nestml in linux install
+#     mamba install gcc_impl_linux-64 gcc_linux-64
+#     mamba install gxx_impl_linux-64 gxx_linux-64
+#
+# To use nest with nestml in osx-64 install
+#     mamba install clang clang-16 clang_impl_osx-64 clang_osx-64 
+#     mamba install clangxx clangxx_impl_osx-64 clangxx_osx-64
+#
+# To use nest with nestml in osx-arm64 install
+#     mamba install clang clang-16 clang_impl_osx-arm64 clang_osx-arm64
+#     mamba install clangxx clangxx_impl_osx-arm64 clangxx_osx-arm64
+
+
+
+BLD_PATH=${dirname "${COMPILER_FULL}"}
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   cp "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config.orig
-  sed -i "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
+  sed -i "s|${COMPILER_FULL}|${COMPILER}|g" "${CONDA_PREFIX}"/bin/nest-config
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   cp "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config.orig
-  sed -i'.bac' "s|${MAC_BLD_ENV}*${MAC_BLD_COMPILER}|${CONDA_PREFIX}/bin/clang++|g" "${CONDA_PREFIX}"/bin/nest-config
+  sed -i'.bac' "s|${COMPILER_FULL}|${COMPILER}|g" "${CONDA_PREFIX}"/bin/nest-config
   sed -i'.bac' "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
 else
   echo $OSTYPE
