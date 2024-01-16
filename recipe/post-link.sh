4f96b7d9ede3ed2f4dc6557ddfee551113f095b2
Steffen Graber <s.graber@fz-juelich.de>
2024-01-16 07:34:36 +0000

    Replace compiler

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -4,13 +4,17 @@ COMPILER_FULL=$(bash nest-config --compiler)
 COMPILER_NAME=$(basename "${CXX}")
 COMPILER_RUN=$(which "${COMPILER_NAME}")
 
+MAC_BLD_ENV="/Users/runner/miniforge3/conda-bld/nest-simulator_"
+MAC_BLD_COMPILER="/_build_env/bin/x86_64-apple-darwin*-clang++"
+MAC_COMPILER="${CONDA_PREFIX}/bin/clang++"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   cp "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config.orig
   sed -i "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i'.orig' "s|${COMPILER_FULL}|${COMPILER_RUN}|g" "${CONDA_PREFIX}"/bin/nest-config
-  sed -i'.orig' "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
+  cp "${CONDA_PREFIX}"/bin/nest-config "${CONDA_PREFIX}"/bin/nest-config.orig
+  sed -i'.bac' "s|${MAC_BLD_ENV}*${MAC_BLD_COMPILER}|${CONDA_PREFIX}/bin/clang++|g" "${CONDA_PREFIX}"/bin/nest-config
+  sed -i'.bac' "s|-fopenmp=libomp|-Xclang -fopenmp|g" "${CONDA_PREFIX}"/bin/nest-config
 else
   echo $OSTYPE
 fi
